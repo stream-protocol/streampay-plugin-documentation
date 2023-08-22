6ad02ca98f0603318e58e5a8616e948a819bb776
Stream Protocol <contact@streamprotocol.org>
2023-08-22 10:28:36 +0000

    edited theme.config: added logo and name

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img src="https://i.imgur.com/zwbfGJs.png" alt="StreamPay Logo" />, // Replace with the actual path to your logo
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/stream-protocol/streampay-plugin-documentation.git',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/zaxTzCuF',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/stream-protocol/streampay-plugin-documentation.git',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2023 Stream Protocol',
   },
-}
+};
 
-export default config
+export default config;
